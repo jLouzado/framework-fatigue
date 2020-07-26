c4b3496bb9d74b1db63d347473b0093e83f216a8
Joel <jlouzado@gmail.com>
2020-07-27 03:09:27 +0530

    feat(pk-mst): extract common fetcher

---
+++ pk-mst/src/models/fetcher/fetcher.model.ts
@@ -0,0 +1,14 @@
+import {types, flow} from 'mobx-state-tree'
+
+export const FetcherModel = types.model({}).actions((self) => ({
+  fetch: flow(function* save() {
+    try {
+      yield fetch('url')
+    } catch (e) {
+      console.error('ERROR: ', e)
+    }
+  }),
+  onResponse() {
+      // save logic
+  }
+}))
